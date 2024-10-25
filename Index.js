2d51558a636563e0472913611787034f5b9435ff
Ashtin <ashtinrivada@yahoo.com>
2024-10-24 19:34:25 -0700

    Made code a lot more readable

--- Index.js
+++ Index.js
@@ -1,57 +1,46 @@
+"use strict";
 
-async function fetchData(){
+ function fetchData(){
     let button = document.getElementById("MemeButton");
     button.addEventListener("click", async () =>{
         try{
-        let {url: memeUrl, id: MemeId} = await getMemeData();
-        ReplaceImg(memeUrl);
+            let api = "https://api.imgflip.com/get_memes"
+            fetch(api) 
+                .then(statusCheck)
+                .then(resp => resp.json())
+                .then(ProcessData)
+                .catch(handleError)
+        
+        
+        
+        //captionMeme(MemeId, "AshRiv", "Riv02$00$",topCaption, botCaption )
 
-        let topCaption = document.getElementById("caption0").value;
-        let botCaption = document.getElementById("caption1").value;
-
-        captionMeme(MemeId, "AshRiv", "Riv02$00$",topCaption, botCaption )
         }catch (error){
             handleError(error);
         }
-
-
-
     });
 }
 
-async function getMemeData(){
-    let resp = await fetch("https://api.imgflip.com/get_memes");
-    await statusCheck(resp); 
-    let data = await resp.json();
-    let url = data.data.memes[0].url;
-    let id = data.data.memes[0].id;
-    return {url, id};
-    
-}
 
 
-function ReplaceImg(url){
+async function ProcessData(data){ 
     let oldImg = document.getElementById("MemePicture");
     let memeContainer = document.getElementById("MemeContainer")
     let newImg = document.createElement("img");
-    newImg.src = url;
+    let topCaption = document.getElementById("caption0").value;
+    let botCaption = document.getElementById("caption1").value;
+    newImg.src = data.data.memes[0].url;
     newImg.alt = "Meme"
     newImg.id = "MemePicture"
 
+
     memeContainer.replaceChild(newImg, oldImg);
-
-
-}
-
-async function captionMeme(memeId, userName, Password, text0, text1){
-    
 }
 
 fetchData();
 
-
 async function handleError(err){
-    console.error(err)
+    alert(console.error(err))
 }
 async function statusCheck(response){
 if(!response.ok){
@@ -59,3 +48,23 @@ if(!response.ok){
 }
 return response; 
 }
+
+// async function captionMeme(memeId, userName, Password, text0, text1){
+    
+// }
+
+// async function getMemeData(){
+//     let api = "https://api.imgflip.com/get_memes"
+//     fetch(api) 
+//         .then(statusCheck)
+//         .then(resp => resp.json())
+//         .then(ProcessData)
+//         .catch(handleError)
+
+
+//     // let resp = await fetch("https://api.imgflip.com/get_memes");
+//     // await statusCheck(resp); 
+//     // let data = await resp.json();
+//     // let url = data.data.memes[0].url;
+//     // let id = data.data.memes[0].id;
+// }
